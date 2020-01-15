f01d929618e606965c8d315851e33f85b3abbd61
denis-brasseur <denis.brasseur@yahoo.fr>
2020-01-15 10:38:33 +0100

    création d'une bibliothèque enigma

---
+++ Enigma.X/enigma.c
@@ -0,0 +1,7 @@
+#include "enigma.h"
+
+rotor* R[3] = {NULL,NULL,NULL};
+
+#define RotorD R[0]
+#define RotorM R[1]
+#define RotorG R[2]
---
+++ Enigma.X/enigma.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   enigma.h
+ * Author: visiteur
+ *
+ * Created on 15 janvier 2020, 10:36
+ */
+
+#ifndef ENIGMA_H
+#define	ENIGMA_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ENIGMA_H */
+
